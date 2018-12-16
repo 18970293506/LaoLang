55c28fc5b5cdec95552c1db4a6c65ed359008a7c
18970293506 <5671405@qq.com>
2018-12-16 16:08:10 +0800

    第一次测试

---
+++ TestFrist/src/main/java/com/laolang/testng/BasicAnnotation.java
@@ -0,0 +1,39 @@
+package com.laolang.testng;
+
+import org.testng.annotations.*;
+
+public class BasicAnnotation {
+
+    @Test
+    public void testCase1(){
+        System.out.println("测试玩玩");
+
+    }
+
+    @Test
+    public void testCase2(){
+        System.out.println("测试用例2");
+    }
+
+    @BeforeMethod
+    public void beforeMethod(){
+        System.out.println("测试之前的步骤");
+    }
+
+    @AfterMethod
+    public void afterMethod(){
+        System.out.println("测试之后的步骤");
+    }
+
+    @BeforeClass
+    public void beforeClass(){
+        System.out.println("类之前步骤");
+    }
+
+    @AfterClass
+    public void afterClass(){
+        System.out.println("类之后步骤");
+    }
+
+
+}
